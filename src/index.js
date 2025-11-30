8b9eb21e471037e5d19d1495f6147c95df98098b
baby4dm <babych.dmtr@gmail.com>
2025-11-30 23:53:44 +0200

    fixed images path

--- src/index.js
+++ src/index.js
@@ -1,30 +1,23 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 const tabData = [
   {
     title: "Bookmark in one click",
     content:
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
-    imageSrc: new URL(
-      "./images/illustration-features-tab-1.svg",
-      import.meta.url
-    ).href,
+    imageSrc: `${BASE_URL}images/illustration-features-tab-1.svg`,
   },
   {
     title: "Intelligent search",
     content:
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
-    imageSrc: new URL(
-      "./images/illustration-features-tab-2.svg",
-      import.meta.url
-    ).href,
+    imageSrc: `${BASE_URL}images/illustration-features-tab-2.svg`,
   },
   {
     title: "Share your bookmarks",
     content:
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
-    imageSrc: new URL(
-      "./images/illustration-features-tab-3.svg",
-      import.meta.url
-    ).href,
+    imageSrc: `${BASE_URL}images/illustration-features-tab-3.svg`,
   },
 ];
 

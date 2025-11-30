391c460a0251fd2c7efb171fbfe72310da102d78
baby4dm <babych.dmtr@gmail.com>
2025-11-30 23:01:18 +0200

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoName = 'bookmark-landing-page'; 
-
 export default defineConfig({
-  base: `/${repoName}/`, 
-  
+  base: "/bookmark-landing-page/",
+
   plugins: [tailwindcss()],
 });

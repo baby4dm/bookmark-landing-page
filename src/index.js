32fa5d59777fbb378fa45927ff6289da70408a83
baby4dm <babych.dmtr@gmail.com>
2025-11-30 23:41:59 +0200

    fixed images path

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const tabData = [
     title: "Bookmark in one click",
     content:
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
-    imageSrc: "./images/illustration-features-tab-1.svg",
+    imageSrc: "/images/illustration-features-tab-1.svg",
   },
   {
     title: "Intelligent search",
     content:
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
-    imageSrc: "./images/illustration-features-tab-2.svg",
+    imageSrc: "/images/illustration-features-tab-2.svg",
   },
   {
     title: "Share your bookmarks",
     content:
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
-    imageSrc: "./images/illustration-features-tab-3.svg",
+    imageSrc: "/images/illustration-features-tab-3.svg",
   },
 ];
 

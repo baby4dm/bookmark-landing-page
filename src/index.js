aad6d86495ba08a184bbc5a576a0674e7d04f683
baby4dm <babych.dmtr@gmail.com>
2025-11-30 23:45:45 +0200

    fixed images path

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ const tabData = [
     title: "Bookmark in one click",
     content:
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
-    imageSrc: "/images/illustration-features-tab-1.svg",
+    imageSrc: new URL(
+      "./images/illustration-features-tab-1.svg",
+      import.meta.url
+    ).href,
   },
   {
     title: "Intelligent search",
     content:
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
-    imageSrc: "/images/illustration-features-tab-2.svg",
+    imageSrc: new URL(
+      "./images/illustration-features-tab-2.svg",
+      import.meta.url
+    ).href,
   },
   {
     title: "Share your bookmarks",
     content:
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
-    imageSrc: "/images/illustration-features-tab-3.svg",
+    imageSrc: new URL(
+      "./images/illustration-features-tab-3.svg",
+      import.meta.url
+    ).href,
   },
 ];
 
@@ -133,7 +142,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
       return;
     }
-
   });
 
   emailInput.addEventListener("input", () => {
